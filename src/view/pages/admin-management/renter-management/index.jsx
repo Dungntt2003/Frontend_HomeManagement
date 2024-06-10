481e6c71481e7485602dddb21f9688e2b85c67f5
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-06-10 14:23:01 +0700

    Update ui inRoom

--- src/view/pages/admin-management/renter-management/index.jsx
+++ src/view/pages/admin-management/renter-management/index.jsx
@@ -1,12 +1,45 @@
 import "./index.scss";
 import React, { useState, useEffect } from "react";
-import { Collapse, theme, List, Avatar, Button, Switch } from "antd";
+import {
+  Collapse,
+  theme,
+  List,
+  Avatar,
+  Button,
+  Switch,
+  Modal,
+  DatePicker,
+} from "antd";
 import inHomeApi from "../../../../api/inRoomApi";
 import { formatDate, formatDateV2 } from "../../../../components/formatDate";
 
 function RenterManagement() {
   const [data, setData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalV2, setModalV2] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
 
+  const showModalV2 = () => {
+    setModalV2(true);
+  };
+  const handleOkV2 = () => {
+    setModalV2(false);
+  };
+  const handleCancelV2 = () => {
+    setModalV2(false);
+  };
+
+  const onChange = (date, dateString) => {
+    console.log(date, dateString);
+  };
   useEffect(() => {
     const getRenters = async () => {
       try {
@@ -42,10 +75,13 @@ function RenterManagement() {
       ),
       children: (
         <div>
-          <div>Danh sách sinh viên trọ tại phòng</div>
+          <div style={{ fontSize: "18px" }}>
+            Danh sách sinh viên trọ tại phòng
+          </div>
           <div>
             <List
               itemLayout="horizontal"
+              style={{ padding: "20px" }}
               dataSource={item.renter}
               renderItem={(renter, index) => (
                 <List.Item>
@@ -72,17 +108,44 @@ function RenterManagement() {
                           </div>
                         </div>
                         <div>
-                          <Button type="primary" size="small">
-                            Tiếp tục gia hạn
+                          <Button
+                            type="primary"
+                            size="small"
+                            onClick={showModal}
+                          >
+                            Thay đổi gia hạn
                           </Button>
                           <Button
                             type="primary"
                             size="small"
                             style={{ marginLeft: "20px" }}
+                            onClick={showModalV2}
                           >
                             Dừng gia hạn
                           </Button>
                         </div>
+                        <Modal
+                          title="Gia hạn"
+                          open={isModalOpen}
+                          onOk={handleOk}
+                          onCancel={handleCancel}
+                        >
+                          <p>Thay đổi gia hạn ở cho {renter.name}</p>
+                          <div>
+                            {" "}
+                            Thời gian gia hạn:{" "}
+                            <DatePicker onChange={onChange} />
+                          </div>
+                        </Modal>
+
+                        <Modal
+                          title="Xác nhận"
+                          open={modalV2}
+                          onOk={handleOkV2}
+                          onCancel={handleCancelV2}
+                        >
+                          <p>Dừng gia hạn ở cho {renter.name}</p>
+                        </Modal>
                       </div>
                     }
                   />
@@ -90,9 +153,10 @@ function RenterManagement() {
               )}
             />
           </div>
-          <div>Danh sách hóa đơn của phòng</div>
+          <div style={{ fontSize: "18px" }}>Danh sách hóa đơn của phòng</div>
           <List
             itemLayout="horizontal"
+            style={{ padding: "20px" }}
             dataSource={item.bill}
             renderItem={(bill, index) => (
               <List.Item>
@@ -142,7 +206,7 @@ function RenterManagement() {
   return (
     <div className="renter-container">
       <div className="renter-header">Quản lý từng phòng</div>
-      <div className="renter-room-list">
+      <div className="renter-room-list" style={{ marginTop: "20px" }}>
         <Collapse
           bordered={false}
           style={{
